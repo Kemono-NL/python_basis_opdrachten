d37c4c2c34b152fe7351dcfbb7f8b11b81165cdc
Kemono-NL <97099449@st.deltion.nl>
2024-03-14 15:37:07 +0100

    14-3 /4

--- opdrachten/080_tekstbestanden/opdr_2/opdr_2.py
+++ opdrachten/080_tekstbestanden/opdr_2/opdr_2.py
@@ -6,11 +6,13 @@ import random
 prompt = "Raad mijn geheime getal \n"
 geheim_getal = random.randint(1, 100)
 getal = 0
+aantal_beurten = 0
 # De rest moet jij doen.....
 while getal != geheim_getal:
     getal = int(input("raad mijn geheime getal: "))
+    aantal_beurten += 1
     if geheim_getal < getal:
-            print(f"Lager")
+            print(f"Lager, dit is beurt {aantal_beurten}")
     elif geheim_getal > getal:
-            print(f"Hoger")
+            print(f"Hoger, Dit is beurt {aantal_beurten}")
 print("Gefeliciteerd!! Ga door voor de wasmaschine!!")
