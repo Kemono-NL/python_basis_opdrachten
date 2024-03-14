46895706fc5994de0654eb3cbd07ff0139ab5f5e
Kemono-NL <97099449@st.deltion.nl>
2024-03-14 15:29:12 +0100

    14-3 /2

--- opdrachten/050_input_function/opdr_1/opdr_1.py
+++ opdrachten/050_input_function/opdr_1/opdr_1.py
@@ -5,30 +5,13 @@
 # Hier komt je code, maak gebruik van de input functie om de lengte van de rechthoekzijden van de driehoek op te vragen.
 
 #a*a + b*b = c*c
+import math
 
-for i in range(3):  
+a = float(input("Geef de lengte van de eerste zijde: "))
+b = float(input("Geef de lengte van de tweede zijde: "))
 
-    print("Geef de lengte van de eerste zijde:")
-    pyth_berekening_str = input()
-    invoer_eerste_str = float(pyth_berekening_str)
-    resultaatA = int(invoer_eerste_str ** 2)
-    
-    print("Geef de lengte van de tweede zijde:")
-    pyth2_berekening_str = input()
-    invoer_tweede_str = float(pyth2_berekening_str)
-    resultaatB = int(invoer_tweede_str ** 2)
+c = round(math.sqrt(pow(a,2) + pow(b,2)),2)
 
-    resultaatC = resultaatA+resultaatB 
-    resultaatD = int(resultaatC ** 0.5)
+print("De lengte van de schuine zijde is:", c)
 
-    if i == 2:  
-        resultaatD = round(resultaatC ** 0.5, 14)
-        print()
-        print(f"De lengte van de schuine zijde is: {resultaatD}")
-        print()
-    else:
-        resultaatD = int(resultaatC ** 0.5)
-        print()
-        print(f"De lengte van de schuine zijde is: {resultaatD}")
-        print()
  
--- opdrachten/080_tekstbestanden/opdr_2/opdr_2.py
+++ opdrachten/080_tekstbestanden/opdr_2/opdr_2.py
@@ -5,6 +5,12 @@
 import random
 prompt = "Raad mijn geheime getal \n"
 geheim_getal = random.randint(1, 100)
-
+getal = 0
 # De rest moet jij doen.....
-
+while getal != geheim_getal:
+    getal = int(input("raad mijn geheime getal: "))
+    if geheim_getal < getal:
+            print(f"Lager")
+    elif geheim_getal > getal:
+            print(f"Hoger")
+print("Gefeliciteerd!! Ga door voor de wasmaschine!!")
