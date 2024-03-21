2b2e6addb3bb44552b5ba4d4702dfbe8952026f5
Kemono-NL <97099449@st.deltion.nl>
2024-03-21 15:17:24 +0100

    21-3-2024

--- opdrachten/095_modules/opdr_2/my_modules/csv.py
+++ opdrachten/095_modules/opdr_2/my_modules/csv.py
@@ -3,7 +3,7 @@
 # In dit bestand komen alle functies.
 # Je kunt de functies in een ander .py bestand gebruiken door te starten  met:
 #from my_modules import csv
-
+#
 
 def sanitize(line):
    new_lst = []
